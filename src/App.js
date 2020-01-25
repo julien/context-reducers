c339347e0e460761722a097f9f9771d22955714c
Julien Castelain <julien.castelain@liferay.com>
2020-01-25 21:32:18 +0100

    Add App.js

--- src/App.js
+++ src/App.js
@@ -1,26 +1,110 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect, useReducer, useState } from "react";
+
+function reducer(state, action) {
+	let nextState = state;
+
+	switch (action.type) {
+		case "add-item":
+			nextState = {
+				...state,
+				todos: [...state.todos, action.todo]
+			};
+			break;
+		case "init":
+			nextState = {
+				...state,
+				todos: [...action.data]
+			};
+			break;
+		case "toggle-item":
+			const n = state.todos.length;
+			if (action.index >= 0 && action.index <= n - 1) {
+				const todo = state.todos[action.index];
+				todo.completed = !todo.completed;
+			}
+			window.localStorage.setItem("todos", JSON.stringify(state.todos));
+
+			nextState = {
+				...state
+			};
+			break;
+		default:
+			nextState = {
+				...state
+			};
+			break;
+	}
+
+	return nextState;
+}
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+	const [title, setTitle] = useState("");
+
+	useEffect(() => {
+		const data = JSON.parse(window.localStorage.getItem("todos") || "[]");
+		if (data && data.length) {
+			dispatch({ data, type: "init" });
+		}
+	}, []);
+
+	const initialState = {
+		todos: []
+	};
+
+	const [state, dispatch] = useReducer(reducer, initialState);
+
+	useEffect(() => {
+		if (state.todos.length) {
+			window.localStorage.setItem("todos", JSON.stringify(state.todos));
+		}
+	}, [state.todos]);
+
+	const handleSubmit = e => {
+		e.preventDefault();
+
+		dispatch({
+			type: "add-item",
+			todo: { completed: false, title }
+		});
+
+		setTitle("");
+	};
+
+	return (
+		<>
+			<form onSubmit={handleSubmit}>
+				<input
+					type="text"
+					name="todo"
+					value={title}
+					onChange={e => {
+						setTitle(e.target.value);
+					}}
+				/>
+			</form>
+			<ul>
+				{state.todos.map((todo, index) => {
+					const className = todo.completed ? "completed" : "incomplete";
+
+					return (
+						<li
+							className={className}
+							key={"todo-" + index}
+							onClick={() => {
+								dispatch({
+									index,
+									type: "toggle-item"
+								});
+							}}
+						>
+							{todo.title}
+						</li>
+					);
+				})}
+			</ul>
+		</>
+	);
 }
 
 export default App;
